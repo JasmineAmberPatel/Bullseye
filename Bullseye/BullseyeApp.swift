7203d6adc131167340f78b60492861ba582bde8f
Jasmine <jasmine.patel@footasylum.com>
2021-05-05 09:51:28 +0100

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Jasmine Patel on 05/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
